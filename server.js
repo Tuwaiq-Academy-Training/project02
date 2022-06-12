b14f9a6cd3b1b1f40d06c9fe286b2cfbdf2d51ac
Ghaida moh <gigi@test.com>
2022-06-12 10:32:53 +0300

    fix the debugERROR(Ghaida)

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-let express=require('express');
+const express=require('express');
 let res = require('express/lib/response');
-let app=express();
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
+const app1=express();
+app1.use(express.urlencoded({extended:false}));
+app1.use(express.json());
 let data=require('./data.json')
 let hw=require('./hw.json' )
 let qAnda=require('./qAnda.json' )
@@ -11,16 +11,13 @@ let RandomCh=require('./RandomCh.json' )
 
 
 
-
-
-
-
 //# for important messages
-app.get('/group/announcement',(req,res)=>{
+app1.get('/group/announcement/:id',(req,res)=>{
     data=data.filter((elm)=>elm.id ==req.params.id);
-     res.json(data);})
+     res.json(data);
+    })
 
-app.post('/group/announcement/create/:id',(req,res)=>{  
+app1.post('/group/announcement/create/:id',(req,res)=>{  
     let newinfo={
             id:req.body.id,
             name:req.body.name,
@@ -30,13 +27,13 @@ app.post('/group/announcement/create/:id',(req,res)=>{
      res.json(data);  });
 
     
- app.put('/group/announcement/update/:id',(req,res)=>{
+ app1.put('/group/announcement/update/:id',(req,res)=>{
         let pepole=data.find((elm)=>elm.id==req.params.id);
         pepole.name=req.body.name;
         data.push(req.body.name);
         res.json(data);  
         });
-    app.delete('/group/announcement/delete/:id',(req,res)=>{
+    app1.delete('/group/announcement/delete/:id',(req,res)=>{
         data=data.filter((elm)=>elm.id !==req.params.id)
         res.json(data);})
 
@@ -46,32 +43,32 @@ app.post('/group/announcement/create/:id',(req,res)=>{
  //# for help with homeworks
 //homeworks
 
-app.get('/group/homeworks/:phase',(req,res)=>{
+app1.get('/group/homeworks/:phase',(req,res)=>{
     hw=hw.filter((elm)=>elm.phase ==req.params.phase);
         res.json(hw);})
 
-app.post('/group/homeworks/create/:phase',(req,res,)=>{
+app1.post('/group/homeworks/create/:phase',(req,res,)=>{
     let newinfo={
         phase:req.body.phase,
         course:req.body.course}  
         hw.push(newinfo);
         res.json(hw);});
 
-app.put('/group/homeworks/update/:phase',(req,res)=>{
+app1.put('/group/homeworks/update/:phase',(req,res)=>{
     let update=hw.find((elm)=>elm.phase==req.params.phase);
     update.hw2=req.body.hw2;
     res.json(hw);});
-app.delete('/group/homeworks/delete/:phase',(req,res)=>{
+app1.delete('/group/homeworks/delete/:phase',(req,res)=>{
      hw=hw.filter((elm)=>elm.phase !==req.params.phase)
      res.json(hw);});
 
 //Q and A
 
-app.get('/group/qAnda/:name',(req,res)=>{
+app1.get('/group/qAnda/:name',(req,res)=>{
     qAnda=qAnda.filter((elm)=>elm.name !==req.params.name);
     res.json(qAnda);})
 
-app.post('/group/qAndA/create/:name',(req,res)=>{
+app1.post('/group/qAndA/create/:name',(req,res)=>{
     let newinfo={
         name:req.body.name,
         course:req.body.course
@@ -81,7 +78,7 @@ app.post('/group/qAndA/create/:name',(req,res)=>{
     res.json(qAnda);})
 
 
-app.put('/group/qAnda/update/:name',(req,res)=>{
+app1.put('/group/qAnda/update/:name',(req,res)=>{
         let person=qAnda.find((elm)=>elm.name==req.params.name);
         person.name=req.body.name;
         data.push(req.body.name);
@@ -89,34 +86,34 @@ app.put('/group/qAnda/update/:name',(req,res)=>{
        
 
         
-app.delete('/group/qAnda/delete/:name',(req,res)=>{
+app1.delete('/group/qAnda/delete/:name',(req,res)=>{
             qAnda=qAnda.filter((elm)=>elm.name !==req.params.name)
             res.json(qAnda);});
 
 
     //random-chatting
 
-app.get('/group/RandomCh/:name',(req,res)=>{
+app1.get('/group/RandomCh/:name',(req,res)=>{
         RandomCh=RandomCh.filter((elm)=>elm.name ==req.params.name);
             res.json(RandomCh);})
   
-    app.post('/group/RandomCh/create/:name',(req,res,)=>{
+    app1.post('/group/RandomCh/create/:name',(req,res,)=>{
         RandomCh.push(req.body);
         res.json(RandomCh);});
     
-        app.put('/group/RandomCh/update/:name',(req,res)=>{
+        app1.put('/group/RandomCh/update/:name',(req,res)=>{
             let random=RandomCh.find((elm)=>elm.name==req.params.name);
             random.messege2=req.body.messege2;
             res.json(RandomCh);});
     
-        app.delete('/group/RandomCh/delete/:name',(req,res)=>{
+        app1.delete('/group/RandomCh/delete/:name',(req,res)=>{
              RandomCh=RandomCh.filter((elm)=>elm.name !==req.params.name)
              res.json(RandomCh);});
     
 
 
 
-app.listen(3000,(err)=>{
+app1.listen(3000,(err)=>{
     if(err) console.log('server is not running');
     console.log('server running on port 3000')
 })
